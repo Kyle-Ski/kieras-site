501d1b53c91f0cdd0cd41b6335fcbc63366d92d1
Kyle-Ski <skiroyjenkins@gmail.com>
2025-07-29 15:52:16 -0700

    reverting that

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
 
-const hostNames = ['cdn.sanity.io', 'images.gr-assets.com']
-
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: hostNames.map((hostname) => ({
-      protocol: 'https',
-      hostname,
-    })),
-  },
-  experimental: {
-    optimizeCss: true
+    domains: ['cdn.sanity.io', 'images.gr-assets.com']
   }
 };
 
